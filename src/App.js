0119d09bba15eec5e190e8ff3ccc24bcc5aaa6dd
Carlos <carlos.anezeh@gmail.com>
2022-01-24 19:09:33 -0600

    Create a form component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
       <h1>Administrador de Citas:</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">
+            <h2>Listado de Citas</h2>
+            <ul>
+              <li>Cita 1</li>
+              <li>Cita 2</li>
+              <li>Cita 3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Formulario = ({crearCita}) => {
+  return (
+    <h2>Manejo de Citas</h2>
+  );
+}
+
+export default Formulario;
+
